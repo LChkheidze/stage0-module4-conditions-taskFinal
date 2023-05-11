4ac3fbbb6e597374c9ebc5d5cb551f7e91140d37
LChkheidze <levanchx@gmail.com>
2023-05-12 00:01:28 +0400

    Switch && bitwise operations

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,29 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if(year < 0 || (month < 0 || month > 12)){
+            System.out.println("invalid date");
+        }else{
+            boolean isLeapYear = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+
+            switch(month) {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    System.out.println(31);
+                    break;
+                case 2:
+                    if(isLeapYear){
+                        System.out.println(29);
+                    }else{
+                        System.out.println(28);
+                    }
+                    break;
+                default:
+                    System.out.println(30);
+                    break;
+            }
+        }
+
+
 
     }
 }
